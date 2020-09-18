5fafa1519e717005face776700032aa0ec1bfffc
suraj1399 <surajkumar068@rediffmail.com>
2020-09-18 18:23:31 +0530

    Intial

---
+++ app/src/androidTest/java/com/piedevelopers/expressouitesting/MainActivityTest.kt
@@ -0,0 +1,47 @@
+package com.piedevelopers.expressouitesting
+
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Test
+
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTest{
+
+
+    @Test
+    fun test_navSecondaryActivity() {
+
+        val activityScenerio= ActivityScenario.launch(MainActivity::class.java)
+
+      onView(withId(R.id.button_next_activity)).perform(click())
+
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+
+
+    }
+
+    @Test
+    fun test_backpressed() {
+
+        val activityScenerio= ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.button_next_activity)).perform(click())
+
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+        onView(withId(R.id.button_back)).perform(click())
+        //pressBack();
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
+
+
+
+    }
+
+
+}
